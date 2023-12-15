ce1adb5d40c37a281ad09f708a3a2bcfc655624e
Piyush Singh <129787142+Piyush02singh@users.noreply.github.com>
2023-12-15 07:28:40 +0000

    datatypes

---
+++ 01.basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict" //use all js code as newer version 
+
+// alert(3+3). // we are using node js not brouser 
+
+console.log(3+3)
+
+console.log("Piyush") //code readability should be veery high 
+
